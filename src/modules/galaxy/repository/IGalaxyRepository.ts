a50932ad5d1f14a12d9eef4ead971e3e7c12792e
rogerxavier29 <85241974+eezsm93@users.noreply.github.com>
2022-07-20 16:17:37 -0300

    ignore node_modules

--- src/modules/galaxy/repository/IGalaxyRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Galaxy } from "../entities/Galaxy";
-
-interface IGalaxyRepository {
-  create(galaxy: Galaxy): Promise<Galaxy>;
-  listAll(): Promise<{ Galaxys: Galaxy[] }>;
-}
-
-export { IGalaxyRepository };
--- src/modules/planets/repository/IPlanetRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Planet } from "../entities/Planet";
-
-interface IPlanetRepository {
-  create(planet: Planet): Promise<Planet>;
-}
-
-export { IPlanetRepository };
